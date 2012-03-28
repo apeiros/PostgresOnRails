fd83e3d0bfedf19d3101b7fbc5719c9b3c02ed89
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-03-28 18:58:59 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostgresOnRails::Application.config.secret_token = '75a6509993e5eec615e589b128144d982e6db5d8ac73840c89360e10342b50de9e391565d8c7da5801e839cb8c0012dc7215ceb64a252a31b22ad14c766d522b'
